4eb2e63643c383d00d9b41f3a4b86c872c106451
Thomas <t.kelly79285@gmail.com>
2021-02-20 15:30:49 -0500

    created play function for api and added separate file for the YTDLSource class

---
+++ YTDL.py
@@ -0,0 +1,48 @@
+import discord
+import youtube_dl
+
+STREAM_PLAYER = None
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': 'TemporaryAudio\\Youtube-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'cachedir': False, # don't use local cache directory
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+        self.title = data.get('title')
+        self.url = data.get('url')
+        self.duration = data.get('duration')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
--- api/app.py
+++ api/app.py
@@ -4,6 +4,10 @@ import config
 from os import listdir
 from os.path import isfile, join, dirname, abspath
 import asyncio
+import glob
+import os
+# from main import STREAM_PLAYER
+import YTDL
 
 class DiscordClient:
     def __init__(self):
@@ -44,6 +48,29 @@ class DiscordClient:
             vc.stop()
             await vc.disconnect()
 
-    async def play(self, youtube_url, user):
+    async def play(self, url, user):
+        voice_channel = None
+        # if the user is not in a voice channel, abort
+        try:
+            voice_channel = self.bot.get_guild(config.GUILD_ID).get_member_named(user).voice.channel
+        except:
+            return
+        # join the voice channel
+        voice_client = await voice_channel.connect()
+        # create a player
+        player = await YTDL.YTDLSource.from_url(url, loop=self.bot.loop)
+        # begin playing music in the channel
+        voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+        # wait for the player to finish playing the youtube url
+        while voice_client.is_playing() or voice_client.is_paused():
+            await asyncio.sleep(1)
 
+        # disconnect from channel
+        voice_client.stop()
+        await voice_client.disconnect()
+        # delete audio file after it is finished playing
+        files = glob.glob("TemporaryAudio/*")
+        for f in files:
+            if 'Youtube-' in f.title():
+                os.remove(f.title())
         return
